bca6eb4eac7bf54cdec11eb35620a94d1e7ac724
Mohammad Arman <arman.cse0@gmail.com>
2026-07-20 01:01:47 +0600

    set prac-1

---
+++ Dictionary and sets/set_prac.py
@@ -0,0 +1,38 @@
+my_set = {1, 2, 3, 4, 5} 
+setss = set()
+setsss = {}
+# print(my_set)
+# print(setss)
+# print(setsss)
+
+my_set.add(6)
+# print(my_set)
+
+my_set.remove(4)
+# print(my_set)
+
+my_set.discard(4)
+# print(my_set)
+
+# my_set.remove(4)
+# print(my_set)
+
+my_set.clear()
+# print(my_set)
+
+my_set = {1, 2, 3, 4, 5}
+your_set = {2, 3, 4, 5}
+# print(my_set.issubset(your_set))
+# print(my_set.issuperset(your_set))
+# print(your_set.issubset(my_set))
+# print(your_set.issuperset(my_set))
+
+dis_set = {23,45,56,54}
+# print(my_set.isdisjoint(dis_set))
+
+# print(my_set | your_set)
+# print(my_set & your_set)
+# print(my_set - your_set)
+# print(my_set ^ your_set)
+# print(your_set - my_set)
+# print(your_set ^ my_set)
