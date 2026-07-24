d02b221c6f5b88824a9c6e26f477fd7758b426af
Mohammad Arman <arman.cse0@gmail.com>
2026-07-24 19:46:54 +0600

    Types of Error

---
+++ Error Handling/Types of error.py
@@ -0,0 +1,16 @@
+# SyntaxError
+# print("Rahim"
+
+# NameError
+# print(rahim)
+
+# TypeError
+# print(5+"5")
+
+# IndexError
+Numbers = [1,2,3]
+# print(Numbers[8])
+
+# AttributeError
+Name = 'Rahim'
+Name.append('uddin')
